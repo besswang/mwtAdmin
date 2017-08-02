b498ce81a606a7fc19bb9dadad7e8b46aa593d63
besswang <licb8568182@163.com>
2017-08-02 18:52:52 +0800

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = { plugins: [ require('autoprefixer')({
+    browsers:['ie>=8','>1% in CN']
+}) ] };
+// var precss = require('precss');
+// var autoprefixer = require('autoprefixer');
+// module.exports = function postcss() {
+//     return [precss, autoprefixer({
+//         remove: false,
+//         browsers: ['ie >= 8', '> 1% in CN'],
+//     })];
+// };
